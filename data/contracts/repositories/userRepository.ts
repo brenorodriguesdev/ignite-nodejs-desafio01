2ad3c241b18835331cdc9545b74933f10096645c
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-11-02 16:32:53 -0300

    feat: add getByUsername on userRepository contract

--- data/contracts/repositories/userRepository.ts
+++ data/contracts/repositories/userRepository.ts
@@ -2,4 +2,5 @@ import UserEntity from "../../entities/user";
 
 export interface UserRepository {
     create: (user: UserEntity) => Promise<void>
+    getByUsername: (username: string) => Promise<UserEntity>
 }
